ea0aa0814d5ebd4e070edbfe7c994f86441c3511
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-01 00:48:37 +0530

    Added jquery.keyframes for dynamic keyframes...Tiny related changes

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
-
 		$(document).ready(function(){
-			alert("Hello");
-			
-			function scroller(){
+			alert("Hello");	
+		});
+
+		function scroller(){
+				alert("clicked");
+				$.keyframe.define([{
+				   	name: 'scrollit',
+				    '20%': {'background-color': 'black'},
+				    '30%': {'background-color': 'white'},
+				    '60%': {'background-color': 'red'},
+				    '100%': {'background-color': 'green'}
+				}]);	
+
 				$(".grid-one").css("animation","scrollit 1s ease 0s 1");
 				$(".grid-two").css("animation","scrollit 1s ease 0.2s 1");
 				$(".grid-three").css("animation","scrollit 1s ease 0.4s 1");
 				$(".grid-four").css("animation","scrollit 1s ease 0.6s 1");
 				$(".grid-five").css("animation","scrollit 1s ease 0.8s 1");
 
-				$(".grid-one").css("animation","initial");
+				/*$(".grid-one").css("animation","initial");
 				$(".grid-two").css("animation","initial");
 				$(".grid-three").css("animation","initial");
 				$(".grid-four").css("animation","initial");
-				$(".grid-five").css("animation","initial");			
-			};
+				$(".grid-five").css("animation","initial");			*/
+		};
 
+		
 
-			$(window).scroll(function(){
-				alert("Checking");
-				scroller();
-			});
+		
 
-		});
