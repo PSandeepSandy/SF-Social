d21fa640e83436fa3c9650fb5f4f8944cb59c043
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-01 14:54:27 +0530

    Animation now working on click, with the delay effect as expected.
    The animation with delay effect is now working. Now what is left to do is to put the Left sliding animation instead of the rotation animation...

--- main.js
+++ main.js
@@ -6,23 +6,29 @@
 				alert("clicked");
 				$.keyframe.define([{
 				   	name: 'scrollit',
-				    '20%': {'background-color': 'black'},
+				    /*'20%': {'transform' : 'matrix(1,0,0,1,0,0)'},
 				    '30%': {'background-color': 'white'},
 				    '60%': {'background-color': 'red'},
-				    '100%': {'background-color': 'green'}
+				    '100%': {'background-color': 'green'}*/
+				    '50%': {'transform' : 'rotate(30deg)'}
 				}]);	
 
-				$(".grid-one").playKeyframe('scrollit 1s ease 0s 1',complete);
-				$(".grid-two").playKeyframe('scrollit 1s ease 0.2s 1',complete);
-				$(".grid-three").playKeyframe('scrollit 1s ease 0.4s 1',complete);
-				$(".grid-four").playKeyframe('scrollit 1s ease 0.6s 1',complete);
-				$(".grid-five").playKeyframe('scrollit 1s ease 0.8s 1',complete);
+					$(".grid-one").playKeyframe('scrollit 1s ease 0s 1');
+					$(".grid-two").playKeyframe('scrollit 1s ease 0.2s 1');
+					$(".grid-three").playKeyframe('scrollit 1s ease 0.4s 1');
+					$(".grid-four").playKeyframe('scrollit 1s ease 0.6s 1');
+					$(".grid-five").playKeyframe('scrollit 1s ease 0.8s 1');
 
-				/*$(".grid-one").css("animation","initial");
-				$(".grid-two").css("animation","initial");
-				$(".grid-three").css("animation","initial");
-				$(".grid-four").css("animation","initial");
-				$(".grid-five").css("animation","initial");			*/
+		
+				setTimeout( function(){ 
+					$(".grid-one").css("animation","initial");
+					$(".grid-two").css("animation","initial");
+					$(".grid-three").css("animation","initial");
+					$(".grid-four").css("animation","initial");
+					$(".grid-five").css("animation","initial");
+ 										}  , 1800 );
+
+
 		};
 
 		
