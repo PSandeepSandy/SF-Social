652063139a72b0f9a80ed5e83f7b06097f94f77a
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-02 01:35:04 +0530

    Animation with left scrolling and revealing worked.
    The backgrounds to be changed and currently the click working for only the first click. to work on it so that it works on subsequent clicks as well...

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
 	
+		var count = 0;
+
 		function scroller(){
-				
+				count++;
+				alert(count);		
 				$.keyframe.define([{
 				   	name: 'scrollit',
 				    '100%': {'transform' : 'translate(100%,0)'}
+					
 				}]);	
 
+					/*$("body").playKeyframe("change".count." 0.2s ease 0s 1 normal forwards");*/
 					$(".grid-one").playKeyframe('scrollit 1s ease 0s 1 normal forwards');
 					$(".grid-two").playKeyframe('scrollit 1s ease 0.2s normal forwards 1');
 					$(".grid-three").playKeyframe('scrollit 1s ease 0.4s 1 normal forwards');
 					$(".grid-four").playKeyframe('scrollit 1s ease 0.6s 1 normal forwards');
 					$(".grid-five").playKeyframe('scrollit 1s ease 0.8s 1 normal forwards');
-
-					setTimeout( function(){														
-					} , 1800);
 		
+					setTimeout(function(){
+						$("body").css("background","url('test2.png')");
+					},200);	
+
+
 				/*setTimeout( function(){ 
 					$(".grid-one").css("animation","initial");
 					$(".grid-two").css("animation","initial");
