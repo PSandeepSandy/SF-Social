697d644df39fe36cbc215437f17eca2b7e57b591
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-01 23:12:25 +0530

    Stopping the div from reverting back to initial position

--- main.js
+++ main.js
@@ -1,33 +1,29 @@
-		$(document).ready(function(){
-			alert("Hello");	
-		});
-
+	
 		function scroller(){
-				alert("clicked");
+				
 				$.keyframe.define([{
 				   	name: 'scrollit',
-				    /*'20%': {'transform' : 'matrix(1,0,0,1,0,0)'},
-				    '30%': {'background-color': 'white'},
-				    '60%': {'background-color': 'red'},
-				    '100%': {'background-color': 'green'}*/
-				    '50%': {'transform' : 'rotate(30deg)'}
+				    '100%': {'transform' : 'translate(100%,0)'}
 				}]);	
 
-					$(".grid-one").playKeyframe('scrollit 1s ease 0s 1');
-					$(".grid-two").playKeyframe('scrollit 1s ease 0.2s 1');
-					$(".grid-three").playKeyframe('scrollit 1s ease 0.4s 1');
-					$(".grid-four").playKeyframe('scrollit 1s ease 0.6s 1');
-					$(".grid-five").playKeyframe('scrollit 1s ease 0.8s 1');
+					$(".grid-one").playKeyframe('scrollit 1s ease 0s 1 normal forwards');
+					$(".grid-two").playKeyframe('scrollit 1s ease 0.2s normal forwards 1');
+					$(".grid-three").playKeyframe('scrollit 1s ease 0.4s 1 normal forwards');
+					$(".grid-four").playKeyframe('scrollit 1s ease 0.6s 1 normal forwards');
+					$(".grid-five").playKeyframe('scrollit 1s ease 0.8s 1 normal forwards');
 
+					setTimeout( function(){														
+					} , 1800);
 		
-				setTimeout( function(){ 
+				/*setTimeout( function(){ 
 					$(".grid-one").css("animation","initial");
 					$(".grid-two").css("animation","initial");
 					$(".grid-three").css("animation","initial");
 					$(".grid-four").css("animation","initial");
 					$(".grid-five").css("animation","initial");
- 										}  , 1800 );
 
+ 										}  , 1800 );*/
+				
 
 		};
 
