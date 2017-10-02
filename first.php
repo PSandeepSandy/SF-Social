ca91d65df5c258dece27c15b0dd19bcb4aa3630f
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-02 11:25:16 +0530

    Added grids in the second page after scrolling...
    The background has to be fixed either by providing separate images using photo editing or by changing the position of background being displayed int he div...

---
+++ first.php
@@ -0,0 +1,11 @@
+<?php
+	echo '	<div class="grids-second grid-second-one"></div>	
+		<div class="grids-second grid-second-two"></div>
+		<div class="grids-second grid-second-three"></div>
+		<div class="grids-second grid-second-four"></div>
+		<div class="grids-second grid-second-five"></div>
+	';
+
+
+
+?>
--- main.js
+++ main.js
@@ -3,19 +3,50 @@
 
 		function scroller(){
 				count++;
-						
+				console.log(count);		
 				$.keyframe.define([{
 				   	name: 'scrollit',
 				    '100%': {'transform' : 'translate(100%,0)'}
 					
 				}]);	
 
-					$("body").css("background","url('test2.png')");
-
-					$(".grid-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards');
-					$(".grid-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards 1');
-					$(".grid-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards');
-					$(".grid-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards');
-					$(".grid-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards');
 					
-		};
+					$(".grid-first-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards');
+					$(".grid-first-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards 1');
+					$(".grid-first-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards');
+					$(".grid-first-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards');
+					$(".grid-first-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards');
+
+					$("body").css("background","url('test2.png')");
+					$("body").css("background-repeat","no-repeat");
+
+					if(count==1){
+						setTimeout(function(){
+							$.ajax({
+							url:"first.php",
+							type:"post",
+							data:{count:count},
+							datatype:"html",
+							success:function(response){
+								$(".main-second-content").html(response);
+								/*$("main-first-content").css("display","none");*/
+							}	
+						});
+						},1500);
+						
+					}
+
+					/*if(count==2){
+						$.ajax({
+							url:"",
+							type:"post",
+							data:{count:count},
+							datatype:"html",
+							success:function(response){
+								$("body").html(response);
+							}	
+						});
+					}*/					
+								
+
+			};
