139227888ecb2710d0e117e1763b57cc2ba94f83
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-03 09:17:40 +0530

    Got the sliding in effect for the next page loading working...
    The sliding in of the next page in grid manner works correctly only for the first scroll...To work on it so that it starts working on all the scrolls/clicks correctly

--- main.js
+++ main.js
@@ -14,9 +14,12 @@
 				   	name: 'scrollthetext',
 					'100%': {'transform' : 'translate(100vw,0)'}
 				}]);
-					/*$("body").css("background","url('test2.png')");
-					$("body").css("background-repeat","no-repeat");*/
 
+				$.keyframe.define([{
+					name: "entering",
+					"100%":{'transform' : 'translate(100vw,0)'},
+				}]);
+				
 					if(count==1){
 						$(".grid-first-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards');
 						$(".grid-first-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards 1');
@@ -26,13 +29,17 @@
 						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
 						$(".main-first-content").playKeyframe('scrollthetext 1s ease 1 normal forwards');
 
-						
-						setTimeout(function(){
+						$(".grid-second-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
+						$(".grid-second-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
+						$(".grid-second-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
+						$(".grid-second-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
+						$(".grid-second-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
+
+						/*setTimeout(function(){
 							$(".main-second-back").css("left","0");
 							$(".main-second-content").css("left","0");	
-						},1800);
-						
-													
+						},1800);*/
+																	
 						
 					}
 
@@ -46,10 +53,16 @@
 						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
 						$(".main-second-content").playKeyframe('scrollthetext 1s ease 1 normal forwards');
 
-						setTimeout(function(){
+						$(".grid-third-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
+						$(".grid-third-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
+						$(".grid-third-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
+						$(".grid-third-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
+						$(".grid-third-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
+
+						/*setTimeout(function(){
 							$(".main-third-back").css("left","0");
 							$(".main-third-content").css("left","0");	
-						},1800);
+						},1800);*/
 
 					}				
 					
@@ -59,14 +72,19 @@
 						$(".grid-third-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards');
 						$(".grid-third-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards');
 						$(".grid-third-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards');	
-
 						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
 						$(".main-third-content").playKeyframe('scrollthetext 1s ease 1 normal forwards');
 
-						setTimeout(function(){
+						$(".grid-fourth-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
+						$(".grid-fourth-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
+						$(".grid-fourth-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
+						$(".grid-fourth-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
+						$(".grid-fourth-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
+
+						/*setTimeout(function(){
 							$(".main-fourth-back").css("left","0");
 							$(".main-fourth-content").css("left","0");	
-						},1800);
+						},1800);*/
 						
 
 					}			
