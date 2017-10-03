9565dfd23f0d3c8cac54efe272a17fa9ecb422ce
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-03 08:40:56 +0530

    Removing the ajax call, working slide-effect and putting the  defining required tags in index itself

--- main.js
+++ main.js
@@ -6,15 +6,13 @@
 				console.log(count);		
 				$.keyframe.define([{
 				   	name: 'scrollit',
-				    '100%': {'transform' : 'translate(100%,0)'}
+				    '100%': {'transform' : 'translate(100vw,0)'}
 					
 				}]);	
 
 				$.keyframe.define([{
 				   	name: 'scrollthetext',
-				    '20%': {'transform' : 'translate(40%,0)'},
-				    '60%': {'transform' : 'translate(80%,0)'},
-					'100%': {'transform' : 'translate(100%,0)'}
+					'100%': {'transform' : 'translate(100vw,0)'}
 				}]);
 					/*$("body").css("background","url('test2.png')");
 					$("body").css("background-repeat","no-repeat");*/
@@ -28,24 +26,13 @@
 						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
 						$(".main-first-content").playKeyframe('scrollthetext 1s ease 1 normal forwards');
 
-						$(".main-second-back").css("left","0");
-						$(".main-second-content").css("left","0");
-						/*setTimeout(function(){
-							$.ajax({
-							url:"first.php",
-							type:"post",
-							data:{count:count},
-							datatype:"html",
-							success:function(response){
-								$("body").html(response);
-							}	
-						});										
-						},1500);*/
-
-						/*setTimeout(function(){
-							$("body").html('<div class="grids-second grid-second-one">ASBASBHe is a very good boy.</div><div class="grids-second grid-second-two">ASBASBHe does not hurt anyone.</div><div class="grids-second grid-second-three">ASBASBHe likes to help others.</div><div class="grids-second grid-second-four">ASBASBHe never thinks about cheating anyone</div><div class="grids-second grid-second-five">ASBASBHe always loves everyone with his whole heart.</div>');
-						},1800);*/
-							
+						
+						setTimeout(function(){
+							$(".main-second-back").css("left","0");
+							$(".main-second-content").css("left","0");	
+						},1800);
+						
+													
 						
 					}
 
@@ -56,17 +43,14 @@
 						$(".grid-second-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards');
 						$(".grid-second-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards');	
 
+						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
+						$(".main-first-content").playKeyframe('scrollthetext 1s ease 1 normal forwards');
 
+						setTimeout(function(){
+							$(".main-third-back").css("left","0");
+							$(".main-third-content").css("left","0");	
+						},1800);
 
-						$.ajax({
-							url:"first.php",
-							type:"post",
-							data:{count:count},
-							datatype:"html",
-							success:function(response){
-								$("body").html(response);
-							}	
-						});
 					}				
 					
 					if(count==3){
@@ -76,16 +60,15 @@
 						$(".grid-third-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards');
 						$(".grid-third-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards');	
 
+						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
+						$(".main-first-content").playKeyframe('scrollthetext 1s ease 1 normal forwards');
 
-						$.ajax({
-							url:"first.php",
-							type:"post",
-							data:{count:count},
-							datatype:"html",
-							success:function(response){
-								$("body").html(response);
-							}	
-						});
+						setTimeout(function(){
+							$(".main-fourth-back").css("left","0");
+							$(".main-fourth-content").css("left","0");	
+						},1800);
+						
+
 					}			
 
 			};
