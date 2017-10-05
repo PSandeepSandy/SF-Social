2c5e8293f7cf0ac4fe6ec1bf4da8cf9d78c5865f
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-05 20:44:10 +0530

    Added the overlay menu for experience sharing, with a field to add new experiences

---
+++ index.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+
+<html lang="en">
+
+<head>
+	<meta charset = "UTF-8">
+	<title>Mental-Health Awareness</title>
+	<link rel="stylesheet" type="text/css" href="styling.css">
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="jquery.keyframes.min.js"></script>
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed|Arapey|Archivo+Black|Elsie+Swash+Caps|Playfair+Display|Anton|Rubik|Teko:600|Sacramento|Abril+Fatface|Cormorant+Garamond|Neucha|Unica+One|Old+Standard+TT" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/decolines.css" />
+	<link rel="stylesheet" type="text/css" href="css/normalize.css" />
+	<link rel="stylesheet" type="text/css" href="css/demo.css" />
+	<link rel="stylesheet" type="text/css" href="css/lettereffect.css" />
+	<link rel="stylesheet" type="text/css" href="css/pater.css" />
+
+</head>
+
+<body onwheel="scroller()">
+	
+	<div class="main-first-back">
+		<div class="grids-first grid-first-one"></div>	
+		<div class="grids-first grid-first-two"></div>
+		<div class="grids-first grid-first-three"></div>
+		<div class="grids-first grid-first-four"></div>
+		<div class="grids-first grid-first-five"></div>
+	</div>
+
+	<div class="main-first-content" id="section3">
+		<button onclick="openNav()" class="tab-bttn">EXPERIENCES</button>
+		<!--<section class="content content--c3" id="section3">
+
+				<div class="slideshow slideshow--3" data-effect="fx2">
+					<div class="slide slide--current">
+						<h2 class="title title--style-3 title--centered">Freedom</h2>
+					</div>
+					<div class="slide">
+						<h2 class="title title--style-3 title--centered">Ba Ba Black Sheep, have you any wool ?</h2>
+					</div>
+					<div class="slide"><h2 class="title title--style-3 title--centered">Justice</h2></div>
+					<div class="slide"><h2 class="title title--style-3 title--centered">Knowledge</h2></div>
+				</div>
+				a
+				<nav class="actions">
+					<button class="btn btn--trigger btn--prev">Previous</button>
+					<button class="btn btn--trigger btn--next">Next</button>
+				</nav>
+
+		</section>-->
+	</div>
+
+	<div class="main-second-back">
+		<div class="grids-second grid-second-one">First Scroll</div>	
+		<div class="grids-second grid-second-two">First Scroll</div>
+		<div class="grids-second grid-second-three">First Scroll</div>
+		<div class="grids-second grid-second-four">First Scroll</div>
+		<div class="grids-second grid-second-five">First Scroll</div>
+	</div>	
+
+	<div class="main-second-content">
+		<p style="color:blue">aqw</p>
+	</div>
+
+	<div class="main-third-back">
+		<div class="grids-third grid-third-one">Second Scroll</div>	
+		<div class="grids-third grid-third-two">Second Scroll</div>
+		<div class="grids-third grid-third-three">Second Scroll</div>
+		<div class="grids-third grid-third-four">Second Scroll</div>
+		<div class="grids-third grid-third-five">Second Scroll</div>
+	</div>	
+
+	<div class="main-third-content">
+		<p style="color:blue">testing</p>
+	</div>
+
+	<div class="main-fourth-back">
+		<div class="grids-fourth grid-fourth-one">Third Scroll</div>	
+		<div class="grids-fourth grid-fourth-two">Third Scroll</div>
+		<div class="grids-fourth grid-fourth-three">Third Scroll</div>
+		<div class="grids-fourth grid-fourth-four">Third Scroll</div>
+		<div class="grids-fourth grid-fourth-five">Third Scroll</div>
+	</div>	
+
+	<div class="main-fourth-content">
+		<p style="color:blue">Let's see</p>
+	</div>
+
+	<!-- The overlay -->
+	<div id="myNav" class="overlay">
+
+  	<!-- Button to close the overlay navigation -->
+  	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+
+  	<!-- Overlay content -->
+  	<div class="overlay-content">
+	    <div>//MESSAGES</div>
+	    
+	    <form action="index.php" method="POST">
+			<textarea rows="4" cols="50" id="exptoadd" type="text" name="exptoadd" placeholder="Share your experience here"></textarea>
+			<input id="submit_btn" type="submit" name="addexp" value="Submit"></input>	    
+	    </form>
+
+  	</div>
+
+	</div>
+
+	<!--Use any element to open/show the overlay navigation menu -->
+
+	<script src="js/charming.min.js"></script>
+	<script src="js/anime.min.js"></script>
+	<script src="js/lineMaker.js"></script>
+	<script src="js/imagesloaded.pkgd.min.js"></script>
+	<script src="js/textfx.js"></script>
+	<script type="text/javascript" src="main.js"></script>
+	
+</body>
+	
+</html>
--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-		
 		var count = 0;
+		var timedOut = 0;
+
+		setTimeout(function(){
+			timedOut = 0;
+		},1600);
 
 		$.keyframe.define([{
 				   	name: 'scrollit',
@@ -33,113 +37,105 @@
 					"100%":{'transform' : 'translate(100vw,0)'}
 				}]);
 
+		var direction=0;
+
+		$('body').bind('wheel', function(e){
+				        if(e.originalEvent.wheelDelta> 0) {
+				            console.log('scrolling up !');
+				            direction = 1; 
+				        	
+				        }
+				        else{
+				            direction = -1;
+				            console.log('scrolling down !');
+				        }
+		});
+
+
 		function scroller(){
 				count++;
-				console.log(count);		
+				console.log(count);	
+	
 				
-					if(count==1){
 
-						$(".grid-first-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards');
-						$(".grid-first-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards 1');
-						$(".grid-first-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards');
-						$(".grid-first-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards');
-						$(".grid-first-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards');	
-						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
-						$(".main-first-content").playKeyframe('scrollthetext 1s ease 1 normal forwards');
 
-						$(".grid-second-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
-						$(".grid-second-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
-						$(".grid-second-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
-						$(".grid-second-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
-						$(".grid-second-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
+				if(direction == -1){
 
-						$(".main-second-content").playKeyframe('enteringtext 1s ease 1 normal forwards');
+					if(timedOut == 0){
 						
-						/*setTimeout(function(){
-							$(".main-second-back").css("left","-100vw");
-							$(".main-second-content").css("left","-100vw");	
-						},1800);				*/
-																	
-						/*$(".grid-first-one").css("animation","initial");
-						$(".grid-first-two").css("animation","initial");
-						$(".grid-first-three").css("animation","initial");
-						$(".grid-first-four").css("animation","initial");
-						$(".grid-first-five").css("animation","initial");
-						$(".main-first-content").css("animation","initial");*/
+						if(count==1){
+
+							$(".grid-first-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards');
+							$(".grid-first-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards 1');
+							$(".grid-first-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards');
+							$(".grid-first-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards');
+							$(".grid-first-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards');	
+							//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
+							$(".main-first-content").playKeyframe('scrollthetext 1s ease 1 normal forwards');
+
+							$(".grid-second-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
+							$(".grid-second-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
+							$(".grid-second-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
+							$(".grid-second-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
+							$(".grid-second-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
+
+							timedOut++;
+						}
+
+						else if(count==2){
+							
+							$(".grid-second-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards initial');
+							$(".grid-second-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards initial');
+							$(".grid-second-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards initial');
+							$(".grid-second-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards initial');
+							$(".grid-second-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards initial');	
+							//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
+							$(".main-second-content").playKeyframe('scrollthetext 1s ease 1 normal forwards initial' );
+
+							$(".grid-third-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
+							$(".grid-third-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
+							$(".grid-third-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
+							$(".grid-third-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
+							$(".grid-third-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
+							
+							timedOut++;
+						}				
 						
+						else if(count==3){
+							$(".grid-third-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards initial');
+							$(".grid-third-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards initial');
+							$(".grid-third-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards initial');
+							$(".grid-third-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards initial');
+							$(".grid-third-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards initial');	
+							//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
+							$(".main-third-content").playKeyframe('scrollthetext 1s ease 1 normal forwards initial');
+
+							$(".grid-fourth-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
+							$(".grid-fourth-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
+							$(".grid-fourth-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
+							$(".grid-fourth-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
+							$(".grid-fourth-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
+							timedOut++;
+						}			
 					}
+				}
 
-					else if(count==2){
-						console.log("I am here");
-						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
-						/*$(".main-first-content").playKeyframe('scrollthetext2 1s ease 1 normal forwards');
-						$(".grid-first-one").playKeyframe('scrollit2 1s ease-in 0s 1 normal forwards');
-						$(".grid-first-two").playKeyframe('scrollit2 0.5s ease 0.2s normal forwards 1');
-						$(".grid-first-three").playKeyframe('scrollit2 1.2s ease-in 0.4s 1 normal forwards');
-						$(".grid-first-four").playKeyframe('scrollit2 0.2s ease 0.6s 1 normal forwards');
-						$(".grid-first-five").playKeyframe('scrollit2 0.6s ease-in 0.8s 1 normal forwards');
-						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
-						$(".main-first-content").playKeyframe('scrollthetext2 1s ease 1 normal forwards');
-						*/
+				else if(direction == 1){
+					//Just the reverse animations...
+				}
 
-						
-						$(".grid-second-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards initial');
-						$(".grid-second-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards initial');
-						$(".grid-second-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards initial');
-						$(".grid-second-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards initial');
-						$(".grid-second-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards initial');	
-												//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
-						$(".main-second-content").playKeyframe('scrollthetext 1s ease 1 normal forwards initial' );
 
-						$(".grid-third-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
-						$(".grid-third-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
-						$(".grid-third-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
-						$(".grid-third-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
-						$(".grid-third-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
-						
-						$(".main-third-content").playKeyframe('enteringtext 1s ease 1 normal forwards');	
-						
-						/*setTimeout(function(){
-							$(".main-third-back").css("left","-100vw");
-							$(".main-third-content").css("left","-100vw");	
-						},1800);*/
 
-						
-						/*$(".grid-second-one").css("animation","initial");
-						$(".grid-second-two").css("animation","initial");
-						$(".grid-second-three").css("animation","initial");
-						$(".grid-second-four").css("animation","initial");
-						$(".grid-second-five").css("animation","initial");*/
 
-						/*$(".grid-second-one").css("animation","initial");
-						$(".grid-second-two").css("animation","initial");
-						$(".grid-second-three").css("animation","initial");
-						$(".grid-second-four").css("animation","initial");
-						$(".grid-second-five").css("animation","initial");*/
 
-					}				
-					
-					else if(count==3){
-						$(".grid-third-one").playKeyframe('scrollit 1s ease-in 0s 1 normal forwards initial');
-						$(".grid-third-two").playKeyframe('scrollit 0.5s ease 0.2s normal forwards initial');
-						$(".grid-third-three").playKeyframe('scrollit 1.2s ease-in 0.4s 1 normal forwards initial');
-						$(".grid-third-four").playKeyframe('scrollit 0.2s ease 0.6s 1 normal forwards initial');
-						$(".grid-third-five").playKeyframe('scrollit 0.6s ease-in 0.8s 1 normal forwards initial');	
-						//TO BE CHANGED AND TO INCORPORATE GRADUAL SCROLL
-						$(".main-third-content").playKeyframe('scrollthetext 1s ease 1 normal forwards initial');
-
-						$(".grid-fourth-one").playKeyframe('entering 1s ease-in 0s 1 normal forwards');
-						$(".grid-fourth-two").playKeyframe('entering 0.5s ease 0.2s normal forwards 1');
-						$(".grid-fourth-three").playKeyframe('entering 1.2s ease-in 0.4s 1 normal forwards');
-						$(".grid-fourth-four").playKeyframe('entering 0.2s ease 0.6s 1 normal forwards');
-						$(".grid-fourth-five").playKeyframe('entering 0.6s ease-in 0.8s 1 normal forwards');
-
-						/*setTimeout(function(){
-							$(".main-fourth-back").css("left","-100vw");
-							$(".main-fourth-content").css("left","-100vw");	
-						},1800);*/
-						
-
-					}			
 
 			};
+
+	function openNav() {
+    	document.getElementById("myNav").style.height = "100%";
+	}
+
+	/* Close */
+	function closeNav() {
+	    document.getElementById("myNav").style.height = "0%";
+	}
