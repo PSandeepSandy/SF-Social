3160880edfdf547312f8534c59399145a2ce8b17
PSandeepSandy <sandy.iitkgp20@gmail.com>
2017-10-07 13:29:11 +0530

    Scroll animation forward part working completely with scroll...Small tweaks required in reverse animation

--- dbconfig/config.php
+++ dbconfig/config.php
@@ -2,8 +2,8 @@
 	$con=mysqli_connect("localhost","root","") or die("Unable to connect");
 	mysqli_select_db($con,"social-messages");
 	
-	$connect=mysqli_connect("localhost","root","") or die("Unable to connect");
-	mysqli_select_db($connect,"social-messages");
+	/*$connect=mysqli_connect("localhost","root","") or die("Unable to connect");
+	mysqli_select_db($connect,"social-messages");*/
 	
 	/*$con=mysqli_connect("2a02:4780:bad:c0de::14","id2166999_webprojectro","Nestle%1") or die("Unable to connect");
 	mysqli_select_db($con,"projectro");
--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 	</div>
 
 	<div class="main-first-content" id="section3">
-		<button onclick="openNav()" class="tab-bttn">EXPERIENCES</button>
+		<!--<button onclick="openNav()" class="tab-bttn">EXPERIENCES</button>-->
 		<!--<section class="content content--c3" id="section3">
 
 				<div class="slideshow slideshow--3" data-effect="fx2">
@@ -45,13 +45,13 @@
 					<div class="slide"><h2 class="title title--style-3 title--centered">Justice</h2></div>
 					<div class="slide"><h2 class="title title--style-3 title--centered">Knowledge</h2></div>
 				</div>
-				a
 				<nav class="actions">
 					<button class="btn btn--trigger btn--prev">Previous</button>
 					<button class="btn btn--trigger btn--next">Next</button>
 				</nav>
 
 		</section>-->
+
 	</div>
 
 	<div class="main-second-back">
@@ -90,71 +90,13 @@
 		<p style="color:blue">Let's see</p>
 	</div>
 
-	<!-- The overlay -->
-	<div id="myNav" class="overlay">
-
-  	<!-- Button to close the overlay navigation -->
-  	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
-
-  	<!-- Overlay content -->
-  	<div class="overlay-content">
-	    <div class="messages">
-	    	<?php
-				if(isset($_POST["addexp"])){
-		    		$exp_message = $_POST["exptoadd"];
-		    		$like_init = 0;
-		    		$query = "INSERT into messages ";
-		    		$query .="(messages,likes) ";
-		    		$query .="VALUES ('{$exp_message}','{$like_init}')";
-		    		$query_run=mysqli_query($con,$query);
-		    		$_POST['exptoadd']=NULL;
-	    		}
-
-	    		$query = "select * from messages";
-	    		$query_run=mysqli_query($con,$query);
-				?>
-				<div class="ind-exp">
-			<?php
-				while($result=mysqli_fetch_assoc($query_run)){
-			?>
-						<cite class="mySlides"><?php echo $result["messages"];?>
-							<br>
-							<?php
-								if($result["likes"] !=0){
-							?>
-							<small><i><?php echo $result["likes"]?> people like this</i></small><?php }?>
-							<button class="likes" onclick="increase_likes()">Like</button>
-						</cite>
-						
-				<?php
-				}
-				?>	
-
-
-					<button class="w3-button w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
-					<button class="w3-button w3-display-right" onclick="plusDivs(+1)">&#10095;</button>
-	    
-	    		</div>
-
-
-
-	    <form action="index.php" method="POST" class="message-input">
-			<textarea rows="4" cols="50" id="exptoadd" type="text" name="exptoadd" placeholder="Share your experience here"></textarea>
-			<input id="submit_btn" type="submit" name="addexp" value="Submit"></input>	    
-	    </form>
-
-  	</div>
-
-	</div>
-
-	<!--Use any element to open/show the overlay navigation menu -->
-
 	<script src="js/charming.min.js"></script>
 	<script src="js/anime.min.js"></script>
 	<script src="js/lineMaker.js"></script>
 	<script src="js/imagesloaded.pkgd.min.js"></script>
 	<script src="js/textfx.js"></script>
 	<script type="text/javascript" src="main.js"></script>
+	<!--<script type="text/javascript" src="effects.js"></script>-->
 
 </body>
 	
